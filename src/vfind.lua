da8c6331f75a98880c336cf6c47a75a4c42ed1d9
NeoHW <neohaowei@gmail.com>
2025-03-03 13:49:33 +0800

    Fix redis script errors

--- src/vfind.lua
+++ src/vfind.lua
@@ -1,11 +1,12 @@
--- Retrieves a range of elements from a sorted set and filters them based on allow, block, and filter sets.
--- Syntax: redis-cli EVAL "$(cat vfind.lua)" <numkeys> <zset_key> [allow_keys...] [blocked_keys...] [filter_keys...] <offset> <count> <upto> <direction> <include_blocked> <allow_count> <block_count> <filter_count>
+-- @desc: Retrieves a range of elements from a sorted set and filters them based on allow, block, and filter sets.
+-- @usage: redis-cli EVAL "$(cat vfind.lua)" <numkeys> <zset_key> [allow_keys...] [blocked_keys...] [filter_keys...] <offset> <count> <upto> <direction> <include_blocked> <allow_count> <block_count> <filter_count>
+-- @return: list of results, and count of results
 
 -- Load the quicksort module
 local quicksort_module = require("quicksort")
 
 -- helper functions
-local function is_in_all_filters(start_index, item)
+local function is_in_all_filters(start_index, item, filter_keys, filter_count)
     for i = start_index, filter_count do
         if redis.call("SISMEMBER", filter_keys[i], item) == 0 then
             return false
@@ -14,8 +15,8 @@ local function is_in_all_filters(start_index, item)
     return true
 end
 
-local function is_blocked(item)
-    for i = start_index, block_count do
+local function is_blocked(item, block_keys, block_count)
+    for i = 1, block_count do
         if redis.call("SISMEMBER", block_keys[i], item) == 1 then
             return true
         end
@@ -23,8 +24,9 @@ local function is_blocked(item)
     return false
 end
 
-local function is_allowed(item)
-    for i = start_index, allow_count do
+local function is_allowed(item, allow_keys, allow_count)
+    if allow_count == 0 then return true end
+    for i = 1, allow_count do
         if redis.call("SISMEMBER", allow_keys[i], item) == 1 then
             return true
         end
@@ -32,49 +34,39 @@ local function is_allowed(item)
     return false
 end
 
-local function compare_sets_by_cardinality(a,b)
+local function compare_sets_by_cardinality(a, b)
     return redis.call("SCARD", a) < redis.call("SCARD", b)
 end
 
-local function vfindBySmallestFilter():
-    results = {}
-    found = 0
-    added = 0
+local function vfindBySmallestFilter(zset_key, filter_keys, filter_count, allow_keys, allow_count, block_keys,
+                                     block_count, offset, count, upto, direction, include_blocked)
+    local results = {}
+    local found, added = 0, 0
+    local zset = redis.call(direction == "desc" and "ZREVRANGE" or "ZRANGE", zset_key, offset, -1)
 
-    local smallest_filter = filter_keys[1]
-    local filter_set = redis.call("ZRANGE", zset_key, offset, -1)
-    if direction == "desc" then
-        filter_set = redis.call("ZREVRANGE", zset_key, offset, -1)
-    end
-
-    for i = 1, #filter_set do
-        local item = filter_set[i]
-
+    for _, item in ipairs(zset) do
         -- check if element exists in the zset
         if redis.call("ZSCORE", zset_key, item) == -1 then
             goto continue
         end
 
-        if filter_count > 0 and not is_in_all_filters(2, item) then
+        if filter_count > 0 and not is_in_all_filters(2, item, filter_keys, filter_count) then
             goto continue
         end
 
-        local is_item_blocked = is_blocked(item)
+        local is_item_blocked = is_blocked(item, block_keys, block_count)
         if is_item_blocked and not include_blocked then
             goto continue
         end
 
-        if allow_count > 0 and not is_allowed(item) then
+        if allow_count > 0 and not is_allowed(item, allow_keys, allow_count) then
             goto continue
         end
 
         -- add found items
         found = found + 1
         if found > offset and added < count then
-            local result = {}
-            result["id"] = item
-            result["blocked"] = is_item_blocked
-            table.insert(results, result)
+            table.insert(results, { id = item, blocked = is_item_blocked })
             added = added + 1
         end
 
@@ -89,39 +81,32 @@ local function vfindBySmallestFilter():
     return results
 end
 
-local function vfindByZSet()
-    results = {}
-    found = 0
-    added = 0
-
-    local zset = redis.call("ZRANGE", zset_key, offset, -1)
-    if direction == "desc" then
-        zset = redis.call("ZREVRANGE", zset_key, offset, -1)
-    end
+local function vfindByZSet(zset_key, filter_keys, filter_count, allow_keys, allow_count, block_keys, block_count, offset,
+                           count, upto, direction, include_blocked)
+    local results = {}
+    local found, added = 0, 0
+    local zset = redis.call(direction == "desc" and "ZREVRANGE" or "ZRANGE", zset_key, offset, -1)
 
     for i = 1, #zset do
         local item = zset[i]
 
-        if filter_count > 0 and not is_in_all_filters(1, item) then
+        if filter_count > 0 and not is_in_all_filters(1, item, filter_keys, filter_count) then
             goto continue
         end
 
-        local is_item_blocked = is_blocked(item)
+        local is_item_blocked = is_blocked(item, block_keys, block_count)
         if is_item_blocked and not include_blocked then
             goto continue
         end
 
-        if allow_count > 0 and not is_allowed(item) then
+        if allow_count > 0 and not is_allowed(item, allow_keys, allow_count) then
             goto continue
         end
 
         -- add found items
         found = found + 1
         if found > offset and added < count then
-            local result = {}
-            result["id"] = item
-            result["blocked"] = is_item_blocked
-            table.insert(results, result)
+            table.insert(results, { id = item, blocked = is_item_blocked })
             added = added + 1
         end
 
@@ -160,15 +145,19 @@ end
 
 local filter_keys = {}
 for i = 1, filter_count do
-    filter_keys[i] = KEYS[allow_count + block_count + i + 1 ]
+    filter_keys[i] = KEYS[allow_count + block_count + i + 1]
 end
 
-quicksort_module.quicksort(filter_keys, compare_sets_by_cardinality)
-size = redis.call("SCARD", filter_keys[1])
-ratio = redis.call("SCARD", zset_key) / size
+if filter_count > 0 and filter_keys[1] then
+    quicksort_module.quicksort(filter_keys, compare_sets_by_cardinality)
+    local size = redis.call("SCARD", filter_keys[1])
+    local ratio = redis.call("SCARD", zset_key) / size
 
-if (size < 100 and ratio > 1) or (size < 500 and ratio > 2) or (size < 2000 and ratio > 3) then
-    return vfindBySmallestFilter()
-else
-    return vfindByZSet()
+    if (size < 100 and ratio > 1) or (size < 500 and ratio > 2) or (size < 2000 and ratio > 3) then
+        return vfindBySmallestFilter(zset_key, filter_keys, filter_count, allow_keys, allow_count, block_keys,
+            block_count, offset, count, upto, direction, include_blocked)
+    end
 end
+
+return vfindByZSet(zset_key, filter_keys, filter_count, allow_keys, allow_count, block_keys, block_count, offset, count,
+    upto, direction, include_blocked)
